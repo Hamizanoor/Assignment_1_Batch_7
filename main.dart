7ec788a24850db505a8d77f3b76358e9c5b5abce
Hamizanoor <hamizanoor991@gmail.com>
2024-08-29 00:09:33 +0500

    addassigmnt1

---
+++ main.dart
@@ -0,0 +1,64 @@
+voidmain() {
+  // 1...You have to print your introduction,
+
+//   print("My name is Hamiza");
+//   print("My father name is Hafeez Ur Rehman");
+//   print("I graduated from the University of Karachi with a degree in Mass Communication");
+//   print("Now i am learning Flutter");
+
+// 2...Assign your Name , Age , Class , percentage , previous result (e.g pass/fail) , according to their data types
+//   String name = " Hamiza";
+//   int age = 25;
+//   num Class = 10;
+//   double percentage = 80.50;
+//   bool previousresult = true;
+//   print("name= $name");
+//   print("age= $age");
+//   print("Class= $Class");
+//   print("percentage= $percentage%");
+//   print(previousresult ? "pass" : "fail");
+
+// 3..Covert Temperature from Fahrenheit to Celsius & Celsius to Fahrenheit as well.
+// °F = (Temperature in Celsius (°C) * 9/5) + 32  .°C = (Temperature in Fahrenheit (°F) - 32) * 5/9
+
+//   var TempinCels = 30;
+//   var F = (TempinCels * 9 / 5) + 32;
+//   print("Celsius to Fahrenheit= $F °F");
+//   var Tempinfahr = 40;
+//   var C = (Tempinfahr - 32) * 5 / 9;
+//   print("Fahrenheit to Celsius= $C °C");
+
+//   4....Length and breadth of a rectangle are 5 and 7 respectively.
+//   Write a program to calculate the area of the rectangle.
+//   Formula (area = length * breadth)
+
+//   int Length = 5;
+//   int breadth = 7;
+//   var area = Length * breadth;
+//   print("area of the rectangle = $area");
+
+//   5....Create a integer variable num = 7, Add 8 to the number and then divide it by 3.
+//   Now, the modulus of that number is taken with 5 and then multiply the resultant value by 5,
+//   store result in variable i. Display the final result.
+
+//   int numb = 7;
+//   var i = (numb + 8 / 3) % 5;
+//   print("i=${i * 5}");
+
+//   6.... If the marks of Robert in three subjects are 78,45 and 62 respectively (each out of 100 ),
+//    write a program to calculate his total marks and percentage marks.
+//    Print his name, marks of all three subjects, total marks and percentage.
+
+//   String name = "Robert";
+//   var maths = 78;
+//   var science = 45;
+//   var english = 62;
+//   var totalmarks = (maths + science + english);
+//   var percentage = (totalmarks * 100 / 300);
+//   print("Name= $name");
+//   print("maths= $maths");
+//   print("science= $science");
+//   print("english = $english");
+//   print("totalmarks= $totalmarks");
+//   print("percentage= $percentage%");
+}
